124339b206320ba4f893513e53ba0d3aa485978c
KAMAL KISHOR <kamalkishor.raj123456789@gmail.com>
2025-03-27 11:09:26 +0530

    Add files via upload

--- connect.js
+++ connect.js
@@ -1,9 +1,8 @@
-const mongoose=require("mongoose");
-mongoose.set("strictQuery",true);
-async function connectToMongoDB(url){
+const  mongoose = require('mongoose')
+async function connectToMongoDB(url) {
     return mongoose.connect(url);
 }
 
-module.exports={
+module.exports= {
     connectToMongoDB,
 }
--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const express=require("express");
-const app=express();
+const express = require('express');
+const  {connectToMongoDB} = require('./connect')
+const URL =  require("./models/url");
+const urlRoute = require('./routes/url')
+const path = require('path')
+const staticRoute = require('./routes/staticRouter')  
 
-const {connectToMongoDB}=require('./connect')
-const URL=require('./model/url');
-const urlRoute=require('./routes/url');
-app.use(express.json())
-app.use('/url',urlRoute);
-app.get('/:shortId',async (req,res)=>{
-    const shortId=req.params.shortId;
+const app=express();
+const PORT=8001;
+
+connectToMongoDB('mongodb+srv://kamalcyber01:vHWvl93jjjPMA5KK@cluster0.qw0xe.mongodb.net/Cyber-verse-urlShort?retryWrites=true&w=majority').then(()=>console.log('MongoDb Connected'))
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+
+app.use("/url",urlRoute);
+
+app.use("/",staticRoute);
+
+app.get('/url/:shortId',async (req,res)=>{
+    const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
-    },{$push:{
-        visitHistory:{ 
+    },  {$push:{
+        visitHistory:{
             timestamp:Date.now(),
-        },
-    }});
-    res.redirect(entry.redirectURL);
-
+        } 
+    },
+}
+);
+res.redirect(entry.redirectURL);
 })
 
-
-connectToMongoDB("mongodb+srv://127.0.0.1/short-url").then(()=>console.log('mongoDb connected'))
-app.listen(8001);
+app.listen(PORT,()=>console.log(`Server  Started at PORT:${PORT}`))
